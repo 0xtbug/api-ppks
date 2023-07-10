aa77f093bd0225bc25f70e4f77d01eaa08f56259
0xtbug <tubagus.nabil19@gmail.com>
2023-07-10 16:01:13 +0700

    update verifyOTP.js

--- helpers/verifyOTP.js
+++ helpers/verifyOTP.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 
-const verifyOTP = async (deviceId, otp, phone) => {
+const verifyOTP = async (deviceId, otp, nomorhp) => {
   try {
     const response = await axios.post("http://47.254.66.241:1337/api/otpverify", { 
         id: deviceId,
         otp: otp,
-        phone: phone,
+        phone: nomorhp
     });
     return {
       isAccepted: response.data.isAccepted,
